7ce8cc398c438eed8c45983190d1da5f0977a05c
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-02 21:34:16 +0300

    add MainPageLocators

---
+++ locators/main_page_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный Кабинет']")
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
+    ORDER_FEED_BUTTON = (By.XPATH, "//p[text()='Лента Заказов']")
