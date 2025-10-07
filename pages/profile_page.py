651beb9f3af76ff368f5d391a5de1166279dff34
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-07 17:51:59 +0300

    add allure steps

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -1,14 +1,17 @@
 from pages.base_page import BasePage
 from locators.profile_page_locators import ProfilePageLocators
-
+import allure
 
 class ProfilePage(BasePage):
+    @allure.step("Клик на кнопку 'История заказов'")
     def click_order_history(self):
         self.click_element(ProfilePageLocators.ORDER_HISTORY_LINK)
 
+    @allure.step("Клик на кнопку 'Выйти'")
     def click_logout(self):
         self.click_element(ProfilePageLocators.LOGOUT_BUTTON)
 
+    @allure.step("Проверка видимости истории заказов")
     def is_order_history_visible(self):
         try:
             self.wait_for_element_visible(ProfilePageLocators.ORDER_HISTORY_LIST)
@@ -16,5 +19,6 @@ class ProfilePage(BasePage):
         except:
             return False
 
+    @allure.step("Проверка выхода из профиля")
     def is_logged_out(self):
         return "login" in self.driver.current_url
