bba5a7906ecea791f8d0c0f954bd752093016c50
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-07 16:43:21 +0300

    add locators

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -21,3 +21,5 @@ class MainPageLocators:
     MODAL = (By.XPATH, "//div[contains(@class, 'Modal_modal')]")
     MODAL_CLOSE_BUTTON = (By.XPATH, "//button[contains(@class, 'Modal_modal__close')]")
     INGREDIENT_DETAILS = (By.XPATH, "//h2[text()='Детали ингредиента']")
+    OVERLAY = (By.XPATH, "//div[contains(@class, 'Modal_modal_overlay')]")
+    ORDER_INDEX = (By.XPATH, "//h2[contains(@class, 'Modal_modal_title')]")
