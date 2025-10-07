a935126993007c8a39d1ee09ee4c0add1a894322
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-07 17:53:57 +0300

    add allure steps

--- pages/forgot_password_page.py
+++ pages/forgot_password_page.py
@@ -1,17 +1,22 @@
+import allure
 from pages.base_page import  BasePage
 from locators.forgot_password_locators import ForgotPasswordLocators
 
 
 class ForgotPasswordPage(BasePage):
+    @allure.step("Ввод email")
     def input_email(self, email):
         self.input_text(ForgotPasswordLocators.EMAIL_INPUT, email)
 
+    @allure.step("Нажать кнопку восстановления пароля")
     def click_restore_button(self):
         self.click_element(ForgotPasswordLocators.RESTORE_BUTTON)
 
+    @allure.step("Нажать кнопку показать пароль")
     def click_show_password_button(self):
         self.click_element(ForgotPasswordLocators.SHOW_PASSWORD_BUTTON)
 
+    @allure.step("Проверка активности поля ввода пароля")
     def is_password_field_active(self):
         try:
             self.wait_for_element_visible(ForgotPasswordLocators.ACTIVE_PASSWORD_FIELD)
@@ -19,6 +24,7 @@ class ForgotPasswordPage(BasePage):
         except:
             return False
 
+    @allure.step("Проверка сброса пароля")
     def restore_password(self, email):
         self.input_email(email)
         self.click_restore_button()
