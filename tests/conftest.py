55aff32f7885fcd6cded954ee4b95e22e94abaa2
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-07 13:59:02 +0300

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,17 @@
 import pytest
 import requests
 from selenium import webdriver
-# from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from api import ApiClient
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from locators.main_page_locators import MainPageLocators
+from locators.login_page_locators import LoginPageLocators
 
 
 # @pytest.fixture(scope="function")
@@ -31,4 +35,20 @@ def driver():
 
 @pytest.fixture(scope="function")
 def create_account():
-    return  requests.post(Urls.REGISTER, json=user_data)
+    client = ApiClient()
+    user = client.user_data()
+    token = client.create_account(user)
+    yield user["email"], user["password"]
+    client.delete_user(token)
+
+
+@pytest.fixture(scope="function")
+def login_user(driver, create_account):
+    email, password = create_account
+    main_page = MainPage(driver)
+    main_page.go_to_site()
+    main_page.click_login_button()
+    login_page = LoginPage(driver)
+    login_page.login(email, password)
+    return driver
+
