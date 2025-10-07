452b176b29ff526c27d1791c7e40fed7acc3f71d
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-07 17:37:48 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,25 @@
 import pytest
-import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.service import Service as FirefoxService
 from api import ApiClient
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-from locators.main_page_locators import MainPageLocators
-from locators.login_page_locators import LoginPageLocators
 
 
-# @pytest.fixture(scope="function")
-# def driver():
-#     options = FirefoxOptions()
-#     options.add_argument('--window-size=1920,1080')
-#     service = Service(GeckoDriverManager().install())
-#     driver = webdriver.Firefox(service=service)
-#     yield driver
-#     driver.quit()
-
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", params=["chrome", "firefox"])
 def driver():
-    options = ChromeOptions()
-    options.add_argument('--window-size=1920,1080')
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    browser = request.param
+    if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument('--window-size=1920,1080')
+        driver = webdriver.Chrome(options=options)
+    elif browser == "firefox":
+        options = FirefoxOptions()
+        options.add_argument('--window-size=1920,1080')
+        driver = webdriver.Firefox(options=options)
+    else:
+        raise ValueError()
     yield driver
     driver.quit()
 
