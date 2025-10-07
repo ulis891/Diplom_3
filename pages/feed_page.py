442fd8608dc41f14ed621878c464b52ac8a3879d
Aleksandr Kostromin <ulis89@yandex.ru>
2025-10-08 00:25:03 +0300

    add steps

--- pages/feed_page.py
+++ pages/feed_page.py
@@ -1,8 +1,11 @@
+import allure
+
 from pages.base_page import BasePage
 from locators.feed_page_locators import FeedPageLocators
 
 
 class FeedPage(BasePage):
+    @allure.step("Клик по заказу")
     def click_order(self, index=0):
         orders = self.find_elements(FeedPageLocators.ORDER_ITEM)
         orders[index].click()
@@ -14,12 +17,15 @@ class FeedPage(BasePage):
         except:
             return False
 
+    @allure.step("Получить количество заказов")
     def get_total_orders_count(self):
         return int(self.get_text(FeedPageLocators.TOTAL_ORDERS))
 
+    @allure.step("Получить количество заказов за сегодня")
     def get_today_orders_count(self):
         return int(self.get_text(FeedPageLocators.TODAY_ORDERS))
 
+    @allure.step("Получить список заказов в работе")
     def get_in_progress_orders(self):
         in_progress_section = self.find_element(FeedPageLocators.IN_PROGRESS_SECTION)
         order_numbers = in_progress_section.find_elements(*FeedPageLocators.ALL_ORDER_NUMBERS)
